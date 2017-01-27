0b459f4f22b2b880086edca946f6d0127d2a443e
tito <tito@cefalo.com>
2017-01-27 16:42:00 +0600

    url spider fix domain

--- myspider.py
+++ myspider.py
@@ -10,52 +10,39 @@ dir_cur=os.path.dirname(os.path.realpath(__file__))+"/"
 
 #print urlparse(sys.argv[1])
 #sys.exit(0)
-
+depth=10
+urls=[]
 
 if len(sys.argv) < 2:
     print "Invalid param, tell which url to crawl."
     sys.exit(0)
 
+if len(sys.argv) >2:
+    depth=int(sys.argv[2])
 
-def getDomain(url):
-    dd=urlparse(url)
-    m = re.search('([a-z0-9|-]+\.)*([a-z0-9|-]+\.[a-z]+)',dd.netloc)
-    ret=dd.netloc
-    if m and m.group(2):
-        ret=m.group(2)
-    return  ret
+domain_name=sys.argv[1]
 
+rls=urlparse(domain_name)
+ll_file=open(dir_cur+rls.netloc+'.urls',"w")
 
-orgin_domain=sys.argv[1]
-domain_name=getDomain(sys.argv[1])
-
-
-ll_file=open(dir_cur+domain_name+'.urls',"w")
-
-print "Parsing domain:"+domain_name
+print "Parsing domain:%s --depth:%d" %(domain_name,depth)
 
 
 link_re = re.compile(r'href="(.*?)"')
 
 def crawl(url, maxlevel):
     #check domain
-    cDom=getDomain(url)
+    urls.append(url)
 
-    if cDom == '':
-        url=orgin_domain+url
-    elif  cDom !=  domain_name:
-        print "[Skip] %s (%s,%s) domian missmatch!" % (url,cDom,domain_name)
+    if not url.startswith(domain_name):
+        print "[Skip] %s (%s) domian missmatch!" % (url,domain_name)
         return False
 
-    ll_file.write(url+"\n")
+   
     if maxlevel == 0:
         return False
     
-    oUrl=urlparse(url)
-    if oUrl.scheme =='https':
-        print "[SKIP] %s reason: https not supported."
-        return False
-
+    ll_file.write(url+"\n")
     # Get the webpage
     req = requests.get(url)
 
@@ -67,9 +54,12 @@ def crawl(url, maxlevel):
     # Find and follow all the links
     links = link_re.findall(req.text)
     for link in links:
-        crawl(link, maxlevel - 1)
+        if link.startswith('/') or link.startswith('#'):
+            link=domain_name+link
+        if link not in urls:
+            crawl(link, maxlevel - 1)
 
-crawl(sys.argv[1], 10)
+crawl(sys.argv[1], depth)
 
 print "Spider Scrapper finished."
 
